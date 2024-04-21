07571717391eb424bd8c4a532fb3dbd8da1fa1b0
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-04-22 00:42:40 +0300

    MainActivity edited

--- app/src/main/java/com/gamzecoskun/bottomnavigationkotlin/MainActivity.kt
+++ app/src/main/java/com/gamzecoskun/bottomnavigationkotlin/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
